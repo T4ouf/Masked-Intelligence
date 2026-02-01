3726b40b6292fbbb1f07d5a74b264c8c9b96507f
Physis21 <juliendudas@gmail.com>
2026-02-01 13:30:16 +0100

    feat: add icon, start json handling

--- Clients/resources/account.gd
+++ Clients/resources/account.gd
@@ -3,4 +3,10 @@ class_name Account
 
 @export var account_image: Texture
 @export var username: String = ""
-@export var description: String = ""
+@export var id = 17
+
+
+## the Dictionnay argument is created by a JSON format received payload
+static func create_account(acc_data: Dictionary):
+	var my_scene: Account = load("res://resources/account.gd")
+	my_scene.username = acc_data.username
---
+++ Clients/scripts/json_handling.gd
@@ -0,0 +1,6 @@
+extends Node
+
+var file = "res://resources/json/player_info_test.json"
+
+func json_to_dict(json_filename: String) -> Dictionary:
+	return JSON.parse_string(FileAccess.get_file_as_string(json_filename))
