4e7145a00ff5233d6a4a3bd6a278fc25b2a20f02
Julien Dudas <julien.dudas@ose-engineering.fr>
2026-03-25 23:11:57 +0100

    feat: style android chat with flex boxes

--- Clients/Web/src/js/androidChat.js
+++ Clients/Web/src/js/androidChat.js
@@ -1,9 +1,51 @@
+import chatState from "./androidChatDefaultMessages.json" with { type: "json" };
+
 const bodyTag = document.body;
 const fontScaleInput = document.querySelector("#fontScale");
+const chatBox = document.querySelector("#chatBox");
+const inputForm = document.querySelector("#inputForm");
 
 const baseSize = parseFloat(window.getComputedStyle(bodyTag).fontSize);
+
+// Functions
+
+function addMessage(message) {
+  const exchangeDiv = document.createElement("div");
+  const messageDiv = document.createElement("div");
+  const promptDiv = document.createElement("div");
+  // Add styles
+  exchangeDiv.classList.add("exchangeContainer");
+  messageDiv.classList.add("messageContainer");
+  promptDiv.classList.add("promptContainer");
+  // Insert answer and prompt
+  messageDiv.innerText = message.answer;
+  promptDiv.innerText = message.prompt;
+  // Add divs to document
+  exchangeDiv.appendChild(promptDiv);
+  exchangeDiv.appendChild(messageDiv);
+  chatBox.appendChild(exchangeDiv);
+}
+
+// Event Listeners
+
+window.addEventListener("load", () => {
+  for (const message of chatState.messages) {
+    console.log(message);
+    addMessage(message);
+  }
+});
 
 fontScaleInput.addEventListener("input", () => {
   const fontScale = fontScaleInput.value;
   bodyTag.style.fontSize = baseSize * fontScale + "px";
+});
+
+inputForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const message = {
+    prompt: "Some prompt",
+    answer: event.target.textInput.value,
+  };
+  console.log(event.target.textInput.value);
+  addMessage(message);
 });
