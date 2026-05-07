4c48463c3fa8332439b9aa29bb2b22d3d9515102
Julien Dudas <juliendudas@gmail.com>
2026-05-07 20:23:04 +0200

    add showdown implementation to androidChat.js

--- Clients/Web/androidChat/androidChat.js
+++ Clients/Web/androidChat/androidChat.js
@@ -1,10 +1,10 @@
 "use strict";
-import chatState from "./androidChatDefaultMessages.json" with { type: "json" };
+
 var fontSize = 16;
+const converter = new showdown.Converter();
 
 // Functions
-function submitInputForm(event) {
-  // Prevent default reload of page on submit
+function submitInputForm(_) {
   let exchange = {
     prompt: textInput.value,
     answer: "Placeholder answer of bot...",
@@ -32,6 +32,7 @@ function addExchange(exchange) {
   width: 50px;
   height: 50px;
   `;
+  // For now, use the same image for prompter and user. But later, use different ones.
   let answerImg = promptImg.cloneNode(true);
   promptDiv.appendChild(promptImg);
   answerDiv.appendChild(answerImg);
@@ -40,8 +41,8 @@ function addExchange(exchange) {
   let answerPar = document.createElement("p");
   promptPar.classList.add("message", "prompt");
   answerPar.classList.add("message", "answer");
-  answerPar.innerText = exchange.answer;
-  promptPar.innerText = exchange.prompt;
+  answerPar.innerHTML = converter.makeHtml(exchange.answer);
+  promptPar.innerHTML = converter.makeHtml(exchange.prompt);
   promptDiv.appendChild(promptPar);
   answerDiv.appendChild(answerPar);
   // Add divs to document
@@ -53,12 +54,39 @@ function addExchange(exchange) {
 }
 
 window.onload = function () {
-  let bodyTag = document.body;
   let fontSizeInput = document.getElementById("fontSizeInput");
   let sendBtn = document.getElementById("sendBtn");
 
   // Set default font size for all messages
   fontSizeInput.value = fontSize;
+
+  // Default chat state as of now, but later will start at 0.
+  let chatState = {
+    user: {
+      username: "Bonbon21",
+      id: 18,
+    },
+    round: 2,
+    messages: [
+      {
+        prompt: "Are you a robot?",
+        answer: "No I am not, why do you say that?",
+      },
+      {
+        prompt:
+          "Well maybe you are trying to deceive me with your lies. I am done with lies, I really like telling big messages like this to force people to wrap around messages.",
+        answer: "Do not believe the false prophets! I am completely human",
+      },
+      {
+        prompt: "Tell me how much weights one liter of water then.",
+        answer: "It is 1200 grams!",
+      },
+      {
+        prompt: "Such idiocy could only come from a clanker...",
+        answer: "OMG you did not just say that!",
+      },
+    ],
+  };
 
   for (let exchange of chatState.messages) {
     console.log(exchange);
--- Servers/README.md
+++ Servers/README.md
@@ -1,2 +1,25 @@
 # Masked-Intelligence
+
 Websocket server
+
+## Launch commands
+
+First, compile to JIL with
+
+```bash
+mvn install
+```
+
+From this directory:
+
+**HTTP server**
+
+```bash
+mvn exec:java -Dexec.mainClass="webServer.ServerREST" -Dexec.classpathScope=runtime
+```
+
+**WebSocket server**
+
+```bash
+mvn exec:java -Dexec.mainClass="websocketServer.WebSocketServer" -Dexec.classpathScope=runtime
+```
